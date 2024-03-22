92619a2af1cbffd0027dd00b2c5ef917dd6061fc
vivek <vivekkashyap043@gmail.com>
2024-03-22 14:39:45 +0530

    login2

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -16,16 +16,11 @@ function Header() {
                 <div className="title">
                     Library Management System
                 </div>
-                <div className="search-bar me-2">
+                <div className="search-bar">
                     <input type="text" placeholder="Search..." />
                     <button>Search</button>
                 </div>
                 <div className='lin'>
-                {/* <div className='nav-item text-white '>
-                    <NavLink className="nav-link" to="">
-                        Home
-                    </NavLink>
-                </div> */}
                 <div className='nav-item text-white'>
                     <NavLink className="nav-link" to="login">
                         Login
